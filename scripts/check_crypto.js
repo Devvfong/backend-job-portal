86d48f0a2d849dd26ad57652c99c99f3c59e8222
Devvfong <ly.soklang.2823@rupp.edu.kh>
2026-05-07 13:53:50 +0700

    chore(gitignore): stop tracking scripts/ and docs/ (keep files locally)

--- scripts/check_crypto.js
+++
@@ -1,9 +0,0 @@
-(async () => {
-  try {
-    const m = await import('../src/utils/crypto.js');
-    console.log('exports:', Object.keys(m));
-  } catch (e) {
-    console.error('error importing crypto:', e);
-    process.exit(1);
-  }
-})();
