f0b63c3d4f34213cc02d6495f40c37d7a003a988
Devvfong <ly.soklang.2823@rupp.edu.kh>
2026-05-24 23:11:12 +0700

    fix: auth

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -3,12 +3,12 @@ import jwt from "jsonwebtoken";
 const generateTokens = (userId, role, res) => {
   // 1. Access Token (Short-lived, eg. 15 mins)
   const accessToken = jwt.sign({ id: userId, role: role }, process.env.JWT_SECRET, {
-    expiresIn: "15m",
+    expiresIn: "5m",
   });
 
-  // 2. Refresh Token (Long-lived, eg. 7 days)
+  // 2. Refresh Token (Long-lived, eg. 1 days)
   const refreshToken = jwt.sign({ id: userId }, process.env.JWT_REFRESH_SECRET || process.env.JWT_SECRET, {
-    expiresIn: "7d",
+    expiresIn: "1d",
   });
 
   // Set the Refresh Token inside the Secure HttpOnly Cookie
@@ -16,7 +16,7 @@ const generateTokens = (userId, role, res) => {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: "strict",
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    maxAge: 1 * 24 * 60 * 60 * 1000, // 1 day
   });
 
   // Only return the Access Token (and optionally the Refresh Token if you need it in controller)
